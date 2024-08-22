4061983996df66b7d018a337468789074ba7e2ea
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-08-22 14:51:42 +0200

    [UPD] Pagination

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,16 @@ export default {
       const result = axios
         .get("http://127.0.0.1:8000/api/posts")
         .then((response) => {
+          this.posts = response.data.results.data;
           console.log(response);
+          console.log(response.data.results.data);
 
-          if (response.data.status && response.data.results.length) {
-            console.log("gestisco i dati in qualche modo");
-            this.projects = response.data.results;
-          } else {
-            console.log("non ci siamo, errore");
-          }
+          // if (response.data.status && response.data.results.length) {
+          //   console.log("gestisco i dati in qualche modo");
+          //   this.projects = response.data.results;
+          // } else {
+          //   console.log("non ci siamo, errore");
+          // }
         })
         .catch((error) => console.log(error));
     },
