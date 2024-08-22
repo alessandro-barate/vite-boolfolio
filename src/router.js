ab33c2e6e2e1693058d732ab2bada44b7ac11979
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-08-22 15:44:55 +0200

    [UPD] Router file edited

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AppHome from "./pages/AppHome.vue";
+import AppAbout from "./pages/AppAbout.vue";
+import PostList from "./pages/PostList.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: AppHome,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AppAbout,
+    },
+    {
+      path: "/blog",
+      name: "posts",
+      component: PostList,
+    },
+  ],
+});
+export { router };
