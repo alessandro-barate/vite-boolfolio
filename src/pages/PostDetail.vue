b851c1b27b97ccf9cd0b460fbc53c95011221536
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-08-25 12:16:14 +0200

    [UPD] Post detail's card styled

--- src/pages/PostDetail.vue
+++ src/pages/PostDetail.vue
@@ -4,6 +4,15 @@ export default {
 };
 </script>
 
-<template>Details</template>
+<template>
+  <h1>Titolo</h1>
+  <p>Content</p>
+  <p>Immagine</p>
+  <div class="btn-width">
+    <router-link class="btn btn-primary" :to="{ name: 'posts' }"
+      >Back to blogs</router-link
+    >
+  </div>
+</template>
 
 <style></style>
